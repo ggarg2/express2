9917b7813b4fdc2c536cb134869082b4f398671c
cruisecoders <icruisecoder007@outlook.com>
2018-02-23 08:35:20 +0530

    aded global midllewares.
    Chain of middlewares
    Export and import middleware
    Skip routes
    Wildcards

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-
 const app = express();
 
+gm3 = require('./middleware/middleware-1')
+
 app.use(bodyParser.json())
+
+app.use((req, res, next)=>{
+    console.log("GLobal Middleware")
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log("GLobal Middleware 1")
+    next();
+})
+
+app.use(gm3);
+
+
 
 console.log("Hello World")
 
@@ -14,20 +29,78 @@ let middleWare1  = (req, res, next) => {
     console.log("auth is ", auth)
     next();
 }
+let middleWare2  = (req, res, next) => {
+    console.log("MiddleWare 2 Run Successfully !!")
+    //console.log(req)
+    let auth = req.headers['authorization'];
+    console.log("auth is ", auth)
+    next();
+}
 
 app.use("/admin", middleWare1)
+app.use("/admin", middleWare2)
 
 app.get('/service-worker.js', (req, res, next) => {
     console.log("service-worker.js")
     res.send("Get method is working")   
 })
 
-app.get('/admin/:id/user/:userId', (req, res, next) => {
+h = (req, res, next) => {
     console.log("User method")
     let id = req.params['id'];
     let userId = req.params['userId'];
-    res.send("User method is working "+id + " userid is "+userId )   
-})
+    //res.send("User method is working "+id + " userid is "+userId ) 
+    next();  
+}
+
+h1 =(req, res, next)=> {
+    console.log("Hello World 1")
+    next(); 
+    }
+h2 = (req, res, next)=> {
+    console.log("Hello World 2")
+    next(); 
+}
+h3 = (req, res, next)=> {
+    console.log("Hello World 3")
+    next();
+}
+h4 = (req, res, next)=> {
+    console.log("Hello World 4")
+    //res.send("HEllo World 4 is called")
+    next();
+}
+
+h5 = (req, res, next)=> {
+    console.log("Hello World 5")
+    next();
+}
+
+h6 = (req, res, next)=> {
+    console.log("Hello World 6")
+    next('route');
+}
+
+h7 = (req, res, next)=> {
+    console.log("Hello World 7")
+    next();
+}
+
+h8 = (req, res, next)=> {
+    console.log("Hello World 8")
+    next();
+}
+
+h9 = (req, res, next)=> {
+    console.log("Hello World 9")
+    res.send("Hello World 9 Called !!")
+}
+
+app.get('/admin/:id/user/:userId', [h1, h2, h3, h4])
+
+app.get('/admin/:id/user/:userId', [h5, h6, h7, h8])
+
+app.get('/admin/:id/user/:userId', [h9])
 
 app.post('/admin', (req, res, next) => {
     let body = req.body;
@@ -35,6 +108,18 @@ app.post('/admin', (req, res, next) => {
     res.send("admin post "+ JSON.stringify(body))
 })
 
+// /ab?de => abde, ade
+// "/ab+de" ==> abde, abbbbbbbbde
+//"/ab*de" ==> abdkjsdhlde
+app.route("/a(bd)?e").get((req, res, next)=> {
+    res.send("Get is working 1")
+}).post((req, res, next) => {
+    res.send("Post is working")
+}).put((req, res, next) => {
+    res.send("Put is working")
+}).delete((req, res, next) => {
+    res.send("delete is working")
+})
 
 app.get('/', (req, res, next) => {
     console.log("Get method is working")
---
+++ middleware/middleware-1.js
@@ -0,0 +1,5 @@
+m1 = (req, res, next) => {
+    console.log("Global middle ware 3")
+}
+
+module.exports = m1;
